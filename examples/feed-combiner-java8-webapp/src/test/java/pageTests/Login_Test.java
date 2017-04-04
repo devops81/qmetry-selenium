bce3392dc6fe372666c4b55a0af6eeaeaf26bbde
devops81 <devops81@gmail.com>
2017-04-05 00:44:59 +0530

    commented after methos and login test method

--- examples/feed-combiner-java8-webapp/src/test/java/pageTests/Login_Test.java
+++ examples/feed-combiner-java8-webapp/src/test/java/pageTests/Login_Test.java
@@ -36,12 +36,12 @@ public class Login_Test {
 	public void IsPasswordFIeldIsPresent() {
 		assertTrue(Login_Page.txtbx_Password(driver).isDisplayed());
 	}
-
+/*
 	@Test
 	public void IsLoginButtonIsPresent() {
 		assertTrue(Login_Page.btn_LogIn(driver).isDisplayed());
 	}
-
+*/
 	@Test
 	public void LoginwithValidCredential() throws InterruptedException {
 		Login_Page.txtbx_UserName(driver).sendKeys("400218542");
@@ -94,9 +94,10 @@ public class Login_Test {
 		driver.get(Login_Page.appUrl);
 	}
 */	
+/*
 	@AfterMethod
 	public void afterMethod() {
 		driver.quit();
 	}
-
+*/
 }
